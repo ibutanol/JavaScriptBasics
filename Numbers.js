0d87beb236148404564c9f1b9edf8312109043fd
ibutanol <makarov.makarov@proton.me>
2024-02-26 07:22:50 +0100

    numbers

---
+++ Numbers.js
@@ -0,0 +1,21 @@
+// JavaScript Arithmetic Operators
+
++	//Addition
+-	//Subtraction
+*	//Multiplication
+**	//Exponentiation
+/	//Division
+%	//Modulus (Remainder)
+++	//Increment
+--	//Decrement
+
+//Examples PS: Before Testing 
+
+let x = 100 + 50;
+console.log(x);
+
+let a = 100;
+let b = 50;
+
+let x = a + b;
+console.log(x);
