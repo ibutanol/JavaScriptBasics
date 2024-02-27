0b6b896d28d0b8dcced341d9e578ea1489950401
ibutanol <makarov.makarov@proton.me>
2024-02-27 06:19:27 +0100

    variables const example

--- Variables.js
+++ Variables.js
@@ -13,3 +13,6 @@ message = "Hello"; //"Hello" (String) is stored in the variable "message"
 let message = "Hello"; // statement + variable + operator + Value
 console.log(message); // shows the variable in the output console
 
+// const are statements which cant be changed (unchanging) variables
+
+const myBirthday = '18.04.1982' // "18.04.1982" cant be changed otherwise an error is thrown
