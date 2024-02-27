21bd22958ec10ea2deeffb0edb48ca6e8b7f266c
ibutanol <makarov.makarov@proton.me>
2024-02-27 06:46:57 +0100

    did some task to keep it in my brain

--- Variables.js
+++ Variables.js
@@ -22,3 +22,43 @@ const myBirthday = '18.04.1982' // "18.04.1982" cant be changed otherwise an err
 let //is a modern variable declaration
 var //is aa old variable declaration
 const //is like let but the variable can't be changed
+
+
+// Task 1
+//1. Declare  two variables: admin and name.
+//2. Assign the value "John" to name.
+//3. Copy the value from name to admin.
+//3. Show the value of admin using alert (must output “John”).
+
+let admin, name;
+name ="John";
+admin = name;
+
+console.log(admin)
+
+// In HTML file this code would look like this:
+<!DOCTYPE html>
+<script>
+"use strict";
+
+let admin, name; // can declare two variables at once
+
+name = "John";
+
+admin = name;
+
+alert( admin ); // "John"
+</script>
+
+// Task 2 
+// 1. Create a variable with the name of our planet. How would you name such a variable?
+// 2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+let ourPlanet, currentVisitor;
+
+ourPlanet = "Bob" // Like the new earthlike planet "Bob" from the movie "TITAN AE"
+
+currentVisitor = "John";
+
+// note: variable names( short, catchy, 3 words max)
+
